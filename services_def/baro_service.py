ec0ca4867b4ecfd64231710b542ec7017dfd7c6c
GODJEONG <GODJEONG>
2024-07-23 15:01:40 +0900

    바로등급 html js 추가

--- services_def/baro_service.py
+++ services_def/baro_service.py
@@ -1,16 +1,44 @@
 
+from typing import List
 from database import SessionLocal
-from sqlalchemy import distinct
-from sqlalchemy.exc import IntegrityError
-from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from models.baro_models import CompanyInfo
 
 
-def get_corp_info(corp_code: str):
+def get_corp_info_code(corp_code: str):
     db: Session = SessionLocal()
     try:
         selectedcompany = db.query(CompanyInfo).filter(CompanyInfo.corp_code == corp_code).first()
         return selectedcompany
     finally:
         db.close()
+        
+def get_corp_info_name(corp_name: str):
+    db: Session = SessionLocal()
+    try:
+        selectedcompany = db.query(CompanyInfo).filter(CompanyInfo.corp_name == corp_name).first()
+        return selectedcompany
+    finally:
+        db.close()
+        
+def get_corp_info_jurir_no(jurir_no: str):
+    db: Session = SessionLocal()
+    try:
+        selectedcompany = db.query(CompanyInfo).filter(CompanyInfo.jurir_no == jurir_no).first()
+        return selectedcompany
+    finally:
+        db.close()
+        
+        
+def get_autocomplete_suggestions(search_type: str, query: str) -> List[str]:
+    db: Session = SessionLocal()
+    if search_type == "corp_name":
+        results = db.query(CompanyInfo.corp_name).filter(CompanyInfo.corp_name.like(f"{query}%")).limit(5).all()
+    elif search_type == "jurir_no":
+        results = db.query(CompanyInfo.jurir_no).filter(CompanyInfo.jurir_no.like(f"{query}%")).limit(5).all()
+    elif search_type == "corp_code":
+        results = db.query(CompanyInfo.corp_code).filter(CompanyInfo.corp_code.like(f"{query}%")).limit(5).all()
+    else:
+        results = []
+
+    return [result[0] for result in results]
